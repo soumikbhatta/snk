3492905260f9978ed34a52e1b442a7b3af2ffe3a
platane <me@platane.me>
2026-01-11 15:34:35 +0100

    bun server

--- packages/demo3/dev.ts
+++ packages/demo3/dev.ts
@@ -1,91 +1,50 @@
-import { type BuildOutput, serve } from "bun";
+import { serve } from "bun";
 import * as childProcess from "child_process";
 import * as fs from "fs";
 import * as path from "path";
 
-//
-// JS
-//
-const Jsbuilder = (() => {
-	let buildPromise: Promise<BuildOutput> | undefined;
+import homepage from "./rr/index.html";
 
-	const build = () =>
-		Bun.build({
-			entrypoints: [__dirname + "/rr/index.html"],
-		});
+const server = serve({
+	routes: {
+		"/": homepage,
+	},
 
-	const invalidate = () => {
-		buildPromise = undefined;
-	};
-	const getArtifacts = async () => {
-		while (true) {
-			if (!buildPromise) buildPromise = build();
-			const b = buildPromise;
-			const output = await b;
-			if (b === buildPromise) return output.outputs;
-		}
-	};
-	return { invalidate, getArtifacts };
-})();
-
-fs.watch(__dirname + "/../", { recursive: true }, (event, filename) => {
-	console.log(`Detected ${event} in ${filename}`);
-	Jsbuilder.invalidate();
+	// Enable development mode for:
+	// - Detailed error messages
+	// - Hot reloading (Bun v1.2.3+ required)
+	development: true,
 });
 
+console.log(`Listening on ${server.url}`);
+
 //
-// Cargo
+// Cargo rebuild
 //
-const cargoBuilder = (() => {
+{
 	const build = () => {
 		const wasmPackFile = path.resolve(
 			__dirname,
 			"../../node_modules/wasm-pack/run.js",
 		);
+		const cwd = path.resolve(__dirname, "../../cargo/snk-js");
 		childProcess.execSync(
 			`${wasmPackFile} build --dev --target web --out-dir ../../packages/snk-js`,
-			{ cwd: path.resolve(__dirname, "../../cargo/snk-js") },
+			{ cwd },
 		);
 	};
 
 	let timeout: number | Timer | undefined;
-	const invalidate = () => {
-		clearTimeout(timeout);
-		timeout = setTimeout(build, 100);
-	};
 
-	return { invalidate };
-})();
+	fs.watch(
+		path.resolve(__dirname, "../../cargo"),
+		{ recursive: true },
+		(_event, filename) => {
+			if (filename?.startsWith("target/")) return;
+			clearTimeout(timeout);
+			timeout = setTimeout(build, 60);
+		},
+	);
 
-fs.watch(
-	path.resolve(__dirname, "../../cargo"),
-	{ recursive: true },
-	(event, filename) => {
-		if (filename?.startsWith("target/")) return;
-		console.log(`Detected ${event} in ${filename}`);
-		cargoBuilder.invalidate();
-	},
-);
-
-cargoBuilder.invalidate();
-
-//
-// http server
-//
-serve({
-	port: 3000,
-	async fetch(request) {
-		let { pathname } = new URL(request.url, "http://localhost");
-
-		if (pathname === "/") pathname = "/index.html";
-
-		const outputs = await Jsbuilder.getArtifacts();
-		const artifact = outputs.find((output) => output.path === `.${pathname}`);
-
-		if (!artifact) return new Response("Not Found", { status: 404 });
-
-		return new Response(artifact);
-	},
-});
-
-console.log("dev server started at", "http://localhost:3000");
+	build();
+}
--- packages/demo3/rr/index.ts
+++ packages/demo3/rr/index.ts
@@ -1,16 +1,3 @@
-// auto-refresh
-fetch("/")
-	.then((res) => res.text())
-	.then(async (original) => {
-		while (true) {
-			await new Promise((resolve) => setTimeout(resolve, 1000));
-			const newContent = await fetch("/").then((res) => res.text());
-			if (newContent !== original) {
-				window.location.reload();
-			}
-		}
-	});
-
 import init, { get_grid_sample, init_panic_hook } from "snk-js";
 import wasmUrl from "snk-js/snk_js_bg.wasm";
 import { createCanvas } from "../utils/canvas";
@@ -21,6 +8,10 @@ init_panic_hook();
 {
 	const grid = get_grid_sample("caves");
 	const { canvas, draw } = createCanvas(grid);
-	draw({ width: grid.width, height: grid.height, data: grid.cells }, [], []);
+	draw(
+		{ width: grid.width, height: grid.height, data: grid.cells },
+		[] as any,
+		[],
+	);
 	document.body.appendChild(canvas);
 }
